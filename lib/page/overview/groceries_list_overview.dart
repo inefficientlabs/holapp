34d03688b5cc5c28c93c297a9ce05c7f2781652f
jokogr <github@jokogr.de>
2026-03-12 17:47:56 +0100

    Some impl

---
+++ lib/page/overview/groceries_list_overview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class GroceriesListOverview extends StatefulWidget {
+  const GroceriesListOverview({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _GroceriesListOverview();
+}
+
+class _GroceriesListOverview extends State<GroceriesListOverview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
