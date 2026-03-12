98692e0cdbf491f08265b569b26585d87aee4496
jokogr <github@jokogr.de>
2026-03-12 21:22:34 +0100

    Implement dedicated app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
+import 'package:holapp/page/overview/groceries_list_overview.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 void main() {
-  runApp(ShadcnApp(title: 'My App', home: Text("Test")));
+  runApp(HolApp());
+}
+
+class HolApp extends StatelessWidget {
+  const HolApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ShadcnApp(title: 'hol', home: GroceriesListOverview());
+  }
 }
